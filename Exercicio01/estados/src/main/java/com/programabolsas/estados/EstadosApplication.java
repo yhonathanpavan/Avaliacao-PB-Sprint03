b309fc9c88ba4ae3ee91869cc3a9e3e3eeed849e
Yhonathan Pavan <yhonathannpavan@gmail.com>
2022-03-16 12:17:53 -0300

    Estrutura inicial com Spring Initializr

---
+++ Exercicio01/estados/src/main/java/com/programabolsas/estados/EstadosApplication.java
@@ -0,0 +1,13 @@
+package com.programabolsas.estados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstadosApplication.class, args);
+	}
+
+}
---
+++ Exercicio01/estados/src/test/java/com/programabolsas/estados/EstadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programabolsas.estados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
