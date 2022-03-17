4bb7f00c34db3f2462f346fe987c34c48a15e0dd
Yhonathan Pavan <yhonathannpavan@gmail.com>
2022-03-17 10:08:24 -0300

    Refatordo o metodo para trabalhar com paginação

--- Exercicio01/estados/src/main/java/com/programabolsas/estados/repository/EstadoRepository.java
+++ Exercicio01/estados/src/main/java/com/programabolsas/estados/repository/EstadoRepository.java
@@ -2,6 +2,8 @@ package com.programabolsas.estados.repository;
 
 import com.programabolsas.estados.modelo.Estado;
 import com.programabolsas.estados.modelo.RegiaoDoEstado;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +13,7 @@ import java.util.List;
 public interface EstadoRepository extends JpaRepository<Estado, Long>{
 
 
-    List<Estado> findByRegiao(RegiaoDoEstado nomeRegiao);
+    Page<Estado> findByRegiao(RegiaoDoEstado nomeRegiao, Pageable paginacao);
 
     List<Estado> findByCapital(String nomeCapital);
 }
