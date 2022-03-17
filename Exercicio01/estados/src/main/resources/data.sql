8191e6b7d67ac8fae9f920278babdbcdfa5c6792
Yhonathan Pavan <yhonathannpavan@gmail.com>
2022-03-17 10:04:33 -0300

    Adicionando mais exemplos na query sql

--- Exercicio01/estados/src/main/resources/data.sql
+++ Exercicio01/estados/src/main/resources/data.sql
@@ -1,3 +1,5 @@
 INSERT INTO ESTADO(nome, regiao, populacao, capital, area) VALUES('Rio Grande do Norte', 'NORDESTE', 3409000, 'Natal', 52797);
 INSERT INTO ESTADO(nome, regiao, populacao, capital, area) VALUES('Rio Grande do Sul', 'SUL', 4908600, 'Porto-Alegre', 74652);
 INSERT INTO ESTADO(nome, regiao, populacao, capital, area) VALUES('São Paulo', 'SUDESTE', 5106773, 'São Paulo', 56482);
+INSERT INTO ESTADO(nome, regiao, populacao, capital, area) VALUES('Minas Gerais', 'SUDESTE', 2006773, 'Belo Horizonte', 586528);
+INSERT INTO ESTADO(nome, regiao, populacao, capital, area) VALUES('Acre', 'NORTE', 2006773, 'Rio Branco', 152581);
