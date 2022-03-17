44e1daed24215d3a4d117377826ff5d5fea952ce
Yhonathan Pavan <yhonathannpavan@gmail.com>
2022-03-17 10:03:38 -0300

    Adição da anotação @EnableSpringDataWebSupport

--- Exercicio01/estados/src/main/java/com/programabolsas/estados/EstadosApplication.java
+++ Exercicio01/estados/src/main/java/com/programabolsas/estados/EstadosApplication.java
@@ -2,8 +2,10 @@ package com.programabolsas.estados;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class EstadosApplication {
 
 	public static void main(String[] args) {
