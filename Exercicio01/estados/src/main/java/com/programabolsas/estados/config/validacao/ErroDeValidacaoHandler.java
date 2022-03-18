da2669db863c94d59941d9177c5c117caec65767
Yhonathan Pavan <yhonathannpavan@gmail.com>
2022-03-18 13:50:36 -0300

    Adição das classes de validação de erros

---
+++ Exercicio01/estados/src/main/java/com/programabolsas/estados/config/validacao/ErroDeValidacaoHandler.java
@@ -0,0 +1,43 @@
+package com.programabolsas.estados.config.validacao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class ErroDeValidacaoHandler {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<ErroDeFormularioDto> handle(MethodArgumentNotValidException exception){
+
+        List<ErroDeFormularioDto> erroDeFormDto = new ArrayList<>();
+        List<FieldError> fieldError = exception.getBindingResult().getFieldErrors();
+
+        fieldError.forEach(e -> {
+
+            String mensagem = messageSource.getMessage(e, LocaleContextHolder.getLocale());
+
+            ErroDeFormularioDto erro = new ErroDeFormularioDto(e.getField(), mensagem);
+            erroDeFormDto.add(erro);
+
+            System.out.println(mensagem);
+            System.out.println(erro);
+            System.out.println(erroDeFormDto);
+        });
+
+        return erroDeFormDto;
+    }
+}
