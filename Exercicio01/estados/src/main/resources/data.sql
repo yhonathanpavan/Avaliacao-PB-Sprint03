bbe6be11342b87ad000043e80fb9e78df304f091
Yhonathan Pavan <yhonathannpavan@gmail.com>
2022-03-19 16:29:00 -0300

    Novos estados adicionados

--- Exercicio01/estados/src/main/resources/data.sql
+++ Exercicio01/estados/src/main/resources/data.sql
@@ -2,4 +2,6 @@ INSERT INTO ESTADO(nome, regiao, populacao, capital, area) VALUES('Rio Grande do
 INSERT INTO ESTADO(nome, regiao, populacao, capital, area) VALUES('Rio Grande do Sul', 'SUL', 4908600, 'Porto-Alegre', 74652);
 INSERT INTO ESTADO(nome, regiao, populacao, capital, area) VALUES('São Paulo', 'SUDESTE', 5106773, 'São Paulo', 56482);
 INSERT INTO ESTADO(nome, regiao, populacao, capital, area) VALUES('Minas Gerais', 'SUDESTE', 2006773, 'Belo Horizonte', 586528);
-INSERT INTO ESTADO(nome, regiao, populacao, capital, area) VALUES('Acre', 'NORTE', 2006773, 'Rio Branco', 152581);
+INSERT INTO ESTADO(nome, regiao, populacao, capital, area) VALUES('Acre', 'NORTE', 790101, 'Rio Branco', 152581);
+INSERT INTO ESTADO(nome, regiao, populacao, capital, area) VALUES('Goias', 'CENTRO_OESTE', 6523005, 'Goiânia', 340086);
+INSERT INTO ESTADO(nome, regiao, populacao, capital, area) VALUES('Rio de Janeiro', 'SUDESTE', 43696, 'Rio de Janeiro', 340086);
